6e193f64ed4f3056e9c371e91291b1fcaee85f32
RhymeLyet <rhymeinparentheses@gmail.com>
2017-09-03 12:58:37 -0700

    Stacks,Arrays,And Stuff

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,17 +1,42 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		Random ran = new Random();
+		for(int i = 0; i < 99; i++) {
+			doubles.push(ran.nextDouble() * 100); 
+		}
+		System.out.println(doubles);
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter a number :"));
+		int num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter another number:"));
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		double num;
+		JOptionPane.showMessageDialog(null, "Popping Elements Off Stack...");
+		for(int i = 0; i < 99; i++) {
+			System.out.println("Working..");
+			num = doubles.pop(); 
+			if(num1 < num2 ) {
+				if(num <= num2 && num >= num1) {
+					JOptionPane.showMessageDialog(null, "" + num);
+					
+				}
+			} else if(num1 > num2) {
+				if(num >= num2 && num <= num1) {
+					JOptionPane.showMessageDialog(null, "" + num);
+				}
+			}
+		}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,20 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<String> savedNums = new Stack<String>();
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +27,46 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	public static void main(String[] args) {
+		TextUndoRedo runner = new TextUndoRedo();
+		runner.run();
+	}
+	public void run() {
+		frame.add(panel);
+		panel.add(label);
+		label.setVisible(true);
+		label.setText("Press Keys!");
+		frame.pack();
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+	
+		if(e.getKeyCode() == 8) {
+			savedNums.push(label.getText().substring(label.getText().length(),label.getText().length()));
+			System.out.println(e.getKeyCode());
+		} else {
+		label.setText(label.getText() + e.getKeyChar());
+		frame.pack();
+		System.out.println(e.getKeyCode());
+		}
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
 	
 }
