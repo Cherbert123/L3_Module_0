93e050a2bd83c3b2a669d66fe5651583057ff08b
Charlie <hallieherbert@yahoo.com>
2017-09-11 15:27:42 -0700

    Completion
    Yay!

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -39,28 +39,6 @@ public class LogSearch implements ActionListener{
 	 * 				is not in the list. 
 	 *
 	 * */
-	JPanel panel = new JPanel(); JFrame frame = new JFrame(); JButton Add = new JButton(); JButton remove = new JButton(); JButton search = new JButton(); JButton view = new JButton(); HashMap<Integer, String> map = new HashMap<Integer, String>();public static void main(String[] arg) {LogSearch search = new LogSearch();search.runner();}public void runner() {frame.add(panel);panel.add(Add);panel.add(search);panel.add(view);panel.add(remove);remove.setText("Remove");view.setText("View List");Add.setText("Add Entry");search.setText("Search by ID");frame.pack();frame.setVisible(true);Add.addActionListener(this);view.addActionListener(this);search.addActionListener(this);remove.addActionListener(this);}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == Add) {
-			map.put(Integer.parseInt(JOptionPane.showInputDialog("Enter an ID Number:")), JOptionPane.showInputDialog("Enter a Name:"));
-		}
-		if(e.getSource() == view) {
-			String list = new String();
-			for(Integer i : map.keySet()) {
-				list = list + "\n ID: " + i + " Name: " + map.get(i);
-			}
-			JOptionPane.showMessageDialog(null, "" + list);
-		}
-		if(e.getSource() == search) {
-			JOptionPane.showMessageDialog(null, map.get(Integer.parseInt(JOptionPane.showInputDialog("Enter ID:"))) );
-		}
-		if(e.getSource() == remove) {
-			map.get(Integer.parseInt());
-			map.replace(JOptionPane.showInputDialog("Enter ID:"), value)
-		}
-		
-	}
-	}
+	JPanel panel = new JPanel(); JFrame frame = new JFrame(); JButton Add = new JButton(); JButton remove = new JButton(); JButton search = new JButton(); JButton view = new JButton(); HashMap<Integer, String> map = new HashMap<Integer, String>();public static void main(String[] arg) {LogSearch search = new LogSearch();search.runner();}public void runner() {frame.add(panel);panel.add(Add);panel.add(search);panel.add(view);panel.add(remove);remove.setText("Remove");view.setText("View List");Add.setText("Add Entry");search.setText("Search by ID");frame.pack();frame.setVisible(true);Add.addActionListener(this);view.addActionListener(this);search.addActionListener(this);remove.addActionListener(this);} @Override public void actionPerformed(ActionEvent e) {if(e.getSource() == Add) {map.put(Integer.parseInt(JOptionPane.showInputDialog("Enter an ID Number:")), JOptionPane.showInputDialog("Enter a Name:"));}if(e.getSource() == view) {String list = new String();for(Integer i : map.keySet()) {list = list + "\n ID: " + i + " Name: " + map.get(i);}JOptionPane.showMessageDialog(null, "" + list);}if(e.getSource() == search) {JOptionPane.showMessageDialog(null, map.get(Integer.parseInt(JOptionPane.showInputDialog("Enter ID:"))) );}if(e.getSource() == remove) {map.remove(Integer.parseInt(JOptionPane.showInputDialog("Enter ID:")));}}}
 
 
